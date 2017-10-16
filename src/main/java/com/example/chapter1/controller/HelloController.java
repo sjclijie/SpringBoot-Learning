41e35af0e44996c72af7ca9f42a7f9896d1c53f9
sjclijie <sjclijie@vip.qq.com>
2017-10-17 00:52:58 +0800

    update

--- src/main/java/com/example/chapter1/controller/HelloController.java
+++ src/main/java/com/example/chapter1/controller/HelloController.java
@@ -1,13 +1,19 @@
 package com.example.chapter1.controller;
 
+import com.example.chapter1.properties.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    private User user;
+
     @GetMapping(value = "/")
     public String index() {
-        return "hello world";
+        return user.getName() + user.getAge();
     }
 }
--- src/test/java/com/example/chapter1/Chapter1ApplicationTests.java
+++ src/test/java/com/example/chapter1/Chapter1ApplicationTests.java
@@ -1,9 +1,12 @@
 package com.example.chapter1;
 
 import com.example.chapter1.controller.HelloController;
+import com.example.chapter1.properties.User;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -21,6 +24,9 @@ public class Chapter1ApplicationTests {
 
     private MockMvc mockMvc;
 
+    @Autowired
+    private User user;
+
     @Before
     public void setUp() throws Exception {
         mockMvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
@@ -30,6 +36,12 @@ public class Chapter1ApplicationTests {
     public void getHello() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("hello world")));
+                .andExpect(content().string(equalTo("lijie22")));
+    }
+
+    @Test
+    public void testProperties() {
+        Assert.assertEquals(user.getName(), "jaylee");
+        Assert.assertEquals((long) user.getAge(), 22);
     }
 }
