2ce1eb7bfa04726b598e634d95821049a51f1a52
sjclijie <sjclijie@vip.qq.com>
2017-10-18 00:32:31 +0800

    add test

---
+++ src/main/java/com/example/chapter1/controller/UserController.java
@@ -0,0 +1,54 @@
+package com.example.chapter1.controller;
+
+import com.example.chapter1.Entry.UserEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    static Map<Long, UserEntry> users = Collections.synchronizedMap(new HashMap<Long, UserEntry>());
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public List<UserEntry> getUserList() {
+
+        List<UserEntry> userEntries = new ArrayList<UserEntry>(users.values());
+
+        return userEntries;
+    }
+
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public UserEntry getUser(@PathVariable("id") Long id) {
+        return users.get(id);
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String addUser(@ModelAttribute UserEntry user) {
+        users.put(user.getId(), user);
+        return "success";
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public String putUser(@PathVariable Long id, @ModelAttribute UserEntry user) {
+
+        UserEntry userEntry = users.get(id);
+
+        userEntry.setName(user.getName());
+        userEntry.setAge(user.getAge());
+
+        users.put(id, userEntry);
+
+        return "success";
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public String deleteUser(@PathVariable Long id) {
+
+        users.remove(id);
+
+        return "success";
+    }
+}
---
+++ src/test/java/com/example/chapter1/UserTests.java
@@ -0,0 +1,76 @@
+package com.example.chapter1;
+
+import com.example.chapter1.controller.UserController;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+
+import javax.print.attribute.standard.Media;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserTests {
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.standaloneSetup(new UserController()).build();
+    }
+
+    @Test
+    public void testUserController() throws Exception {
+
+        RequestBuilder request = null;
+
+        //get
+        request = MockMvcRequestBuilders.get("/users/");
+
+        mockMvc.perform(request).andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[]")));
+
+        //post
+        request = MockMvcRequestBuilders.post("/users/")
+                .param("id", "1")
+                .param("name", "lijie")
+                .param("age", "20")
+                .accept(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(request).andExpect(status().isOk())
+                .andExpect(content().string(equalTo("success")));
+
+        //get
+        request = MockMvcRequestBuilders.get("/users/").accept(MediaType.APPLICATION_JSON);
+        mockMvc.perform(request).andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[{\"id\":1,\"name\":\"lijie\",\"age\":20}]")));
+
+        //put
+        request = MockMvcRequestBuilders.put("/users/1")
+                .param("name", "aaa")
+                .param("age", "22")
+                .accept(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(request).andExpect(status().isOk())
+                .andExpect(content().string(equalTo("success")));
+
+        //get
+        request = MockMvcRequestBuilders.get("/users/1")
+                .accept(MediaType.APPLICATION_JSON);
+        mockMvc.perform(request).andExpect(status().isOk())
+                .andExpect(content().string(equalTo("{\"id\":1,\"name\":\"aaa\",\"age\":22}")));
+    }
+}
