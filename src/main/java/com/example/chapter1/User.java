1d9ba345491868579c72e52b957dcde8e79b11db
sjclijie <sjclijie@vip.qq.com>
2017-10-24 23:58:11 +0800

    update

---
+++ src/main/java/com/example/chapter1/User.java
@@ -0,0 +1,49 @@
+package com.example.chapter1;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Integer Id;
+
+    @Column(nullable = false)
+    private String Name;
+
+    @Column(nullable = false)
+    private Integer Age;
+
+    public User(String name, Integer age) {
+        Name = name;
+        Age = age;
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Integer getAge() {
+        return Age;
+    }
+
+    public void setAge(Integer age) {
+        Age = age;
+    }
+}
