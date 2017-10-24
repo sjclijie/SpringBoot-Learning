39242a63ef0c6cc28b11d74f40f3a7a2517d3e48
sjclijie <sjclijie@vip.qq.com>
2017-10-24 23:23:55 +0800

    update

--- src/main/java/com/example/chapter1/Entry/UserEntry.java
+++ src/main/java/com/example/chapter1/Entry/UserEntry.java
@@ -1,10 +1,27 @@
 package com.example.chapter1.Entry;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class UserEntry {
 
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private Integer age;
+
+    public UserEntry(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
 
     public Long getId() {
         return id;
